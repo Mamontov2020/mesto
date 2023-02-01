c52a4b9e622a9081aaeb82460c1c8f1ff23f5ade
alex <a.mamontov1990@gmail.com>
2023-02-01 10:28:04 +0300

    1.02.23

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-const popupElement = document.querySelector('.popup');
+const popupElement = document.querySelector(".popup");
+const popupBtnOpen = document.querySelector(".profile__edit");
+const popupBtnClose = document.querySelector(".popup__close");
+
+popupBtnOpen.addEventListener("click", openPopup);
+popupBtnClose.addEventListener("click", closePopup);
+
+function openPopup() {
+    popupElement.classList.add("popup__is-opened");
+}
+
+function closePopup() {
+    popupElement.classList.remove("popup__is-opened");
+}

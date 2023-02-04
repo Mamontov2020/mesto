b3d635f481c4fe9a3e77ea7aa069852ac239aed0
Alex Mamontov <a.mamontov1990@gmail.com>
2023-02-04 22:50:02 +0300

    04.02.23

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,34 @@
 const popupElement = document.querySelector(".popup");
 const popupBtnOpen = document.querySelector(".profile__edit");
 const popupBtnClose = document.querySelector(".popup__close");
+const popupBtnSave = document.querySelector(".popup__save");
+const profileNameElement = document.querySelector(".profile__name");
+const profileProfessionElement = document.querySelector(".profile__profession");
+const nameElement = document.querySelector("#name");
+const professionElement = document.querySelector("#profession");
 
-popupBtnOpen.addEventListener("click", openPopup);
-popupBtnClose.addEventListener("click", closePopup);
+
 
 function openPopup() {
-    popupElement.classList.add("popup__is-opened");
+    nameElement.value = profileNameElement.textContent;
+    professionElement.value = profileProfessionElement.textContent;
+    popupElement.classList.add("popup__is_opened");
 }
 
 function closePopup() {
-    popupElement.classList.remove("popup__is-opened");
+    popupElement.classList.remove("popup__is_opened");
 }
+
+function savePopup() {
+    if (nameElement.value !== "" && professionElement.value !== "") {
+        profileNameElement.textContent = nameElement.value;
+        profileProfessionElement.textContent = professionElement.value;
+        closePopup();
+    }
+    
+}
+
+
+popupBtnOpen.addEventListener("click", openPopup);
+popupBtnClose.addEventListener("click", closePopup);
+popupBtnSave.addEventListener("click", savePopup);

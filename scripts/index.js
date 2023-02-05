3a87ea67e1ea56dc6f7e3a7a4d5e55db76d98064
alex <a.mamontov1990@gmail.com>
2023-02-05 19:58:46 +0300

    05.02.23

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const popupElement = document.querySelector(".popup");
 const popupBtnOpen = document.querySelector(".profile__edit");
 const popupBtnClose = document.querySelector(".popup__close");
-const popupBtnSave = document.querySelector(".popup__save");
+const profileFormElement = document.querySelector("#popup__profile");
 const profileNameElement = document.querySelector(".profile__name");
 const profileProfessionElement = document.querySelector(".profile__profession");
 const nameElement = document.querySelector("#name");
@@ -19,16 +19,16 @@ function closePopup() {
     popupElement.classList.remove("popup__is-opened");
 }
 
-function savePopup() {
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
     if (nameElement.value !== "" && professionElement.value !== "") {
         profileNameElement.textContent = nameElement.value;
         profileProfessionElement.textContent = professionElement.value;
         closePopup();
     }
-    
 }
 
 
 popupBtnOpen.addEventListener("click", openPopup);
 popupBtnClose.addEventListener("click", closePopup);
-popupBtnSave.addEventListener("click", savePopup);
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
